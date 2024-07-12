9792c4c965afb2061ce0e00f7da8289cb2558052
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2024-07-12 15:15:58 +0300

    add spawn restrictions & use vanilla nether tag

--- common/src/main/java/net/pixeldreamstudios/beings_of_elderia/registry/EntityRegistry.java
+++ common/src/main/java/net/pixeldreamstudios/beings_of_elderia/registry/EntityRegistry.java
@@ -2,17 +2,22 @@ package net.pixeldreamstudios.beings_of_elderia.registry;
 
 import dev.architectury.registry.level.biome.BiomeModifications;
 import dev.architectury.registry.level.entity.EntityAttributeRegistry;
+import dev.architectury.registry.level.entity.SpawnPlacementsRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.pixeldreamstudios.beings_of_elderia.BeingsOfElderia;
 import net.pixeldreamstudios.beings_of_elderia.entity.DemonEntity;
 import net.pixeldreamstudios.beings_of_elderia.entity.ImpEntity;
 import net.pixeldreamstudios.beings_of_elderia.entity.ImpGuardEntity;
+import net.pixeldreamstudios.beings_of_elderia.entity.abstraction.AbstractDemonEntity;
 
 public class EntityRegistry {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(BeingsOfElderia.MOD_ID, Registries.ENTITY_TYPE);
@@ -33,9 +38,14 @@ public class EntityRegistry {
                     .build(new ResourceLocation(BeingsOfElderia.MOD_ID, "imp_guard").toString()));
 
     private static void initSpawns() {
-        BiomeModifications.addProperties(b -> b.hasTag(TagRegistry.NETHER_BIOMES), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(DEMON.get(), 5, 1, 5)));
-        BiomeModifications.addProperties(b -> b.hasTag(TagRegistry.NETHER_BIOMES), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(IMP.get(), 5, 1, 5)));
-        BiomeModifications.addProperties(b -> b.hasTag(TagRegistry.NETHER_BIOMES), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(IMP_GUARD.get(), 5, 1, 5)));
+        SpawnPlacementsRegistry.register(EntityRegistry.DEMON, SpawnPlacements.Type.NO_RESTRICTIONS, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, AbstractDemonEntity::checkAnyLightMonsterSpawnRules);
+        BiomeModifications.addProperties(b -> b.hasTag(BiomeTags.IS_NETHER), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(DEMON.get(), 20, 1, 6)));
+
+        SpawnPlacementsRegistry.register(EntityRegistry.IMP, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, AbstractDemonEntity::checkAnyLightMonsterSpawnRules);
+        BiomeModifications.addProperties(b -> b.hasTag(BiomeTags.IS_NETHER), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(IMP.get(), 10, 1, 6)));
+
+        SpawnPlacementsRegistry.register(EntityRegistry.IMP_GUARD, SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, AbstractDemonEntity::checkAnyLightMonsterSpawnRules);
+        BiomeModifications.addProperties(b -> b.hasTag(BiomeTags.IS_NETHER), (ctx, b) -> b.getSpawnProperties().addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(IMP_GUARD.get(), 10, 1, 6)));
     }
 
     private static void initAttributes() {
